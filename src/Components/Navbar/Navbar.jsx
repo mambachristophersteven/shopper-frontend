19a68a3029c3203f402b6a0b6adc88286a24a308
Christopher Steven <christopherstevennunoo@gmail.com>
2024-01-14 22:26:06 +0000

    navbar content added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../Assets/logo.png'
+import cart_icon from '../../Assets/cart_icon.png'
 export const Navbar = () => {
   return (
-    <div className='navbar'>My Navbar</div>
+    <div className='navbar'>
+      <div className="nav-logo">
+        <img src={logo} alt="logo" />
+        <p className="logo-name">SHOPPER</p>
+      </div>
+      <ul className="nav-menu">
+        <li>Shop</li>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <img src={cart_icon} alt="cart icon" />
+      </div>
+    </div>
   )
 }

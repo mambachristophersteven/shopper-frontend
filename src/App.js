ea0c7647124b76131554a359934a4797c522043d
Christopher Steven <christopherstevennunoo@gmail.com>
2024-01-17 15:57:56 +0000

    routing pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Shop } from './Pages/Shop';
+import { ShopCategory } from './Pages/ShopCategory';
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>} />
+          <Route path='/mens' element={<ShopCategory category="men"/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

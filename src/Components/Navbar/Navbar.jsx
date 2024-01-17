48f6fb0c0df2c13da2eafae4f4511c0d96f88353
Christopher Steven <christopherstevennunoo@gmail.com>
2024-01-17 22:04:49 +0000

    linking navbar menu list to pages

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import logo from '../../Assets/logo.png'
 import cart_icon from '../../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
 export const Navbar = () => {
 
   const [menu,setMenu] = useState("shop");
@@ -12,10 +13,10 @@ export const Navbar = () => {
         <p className="logo-name">SHOPPER</p>
       </div>
       <ul className="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}>Shop{menu === "shop"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("mens")}}>Men{menu === "mens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("womens")}}>Women{menu === "womens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}>Kids{menu === "kids"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("shop")}}><Link to='/'>Shop</Link>{menu === "shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}}><Link to='/mens'>Men</Link>{menu === "mens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("womens")}}><Link to='/womens'>Women</Link>{menu === "womens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}><Link to='kids'>Kids</Link>{menu === "kids"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
         <button>login</button>

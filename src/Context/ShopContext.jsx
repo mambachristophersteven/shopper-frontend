27054af56a317e76bef015af0dd6cef4ab0f98fd
Christopher Steven <christopherstevennunoo@gmail.com>
2024-01-31 15:06:37 +0000

    adding of ShopContext context file

---
+++ src/Context/ShopContext.jsx
@@ -0,0 +1,9 @@
+import React, { createContext } from "react";
+import all_product from '../Assets/all_product'
+
+
+export const ShopContext = createContext(null);
+
+const ShopContextProvider = (props) => {
+    const contextValue = {}
+}

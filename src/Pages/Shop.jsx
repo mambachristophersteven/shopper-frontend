90b003c67b8ff9280ec4a8b270d8a21446e8eb6b
Christopher Steven <christopherstevennunoo@gmail.com>
2024-01-31 16:03:47 +0000

    removed navbar from shop

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -4,7 +4,6 @@ import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
 import NewCollections from '../Components/NewCollections/NewCollections'
 import NewsLetter from '../Components/NewsLetter/NewsLetter'
-import { Navbar } from '../Components/Navbar/Navbar'
 
 export const Shop = () => {
   return (
@@ -14,7 +13,6 @@ export const Shop = () => {
       <Offers/>
       <NewCollections/>
       <NewsLetter/>
-      <Navbar/>
     </div>
   )
 }

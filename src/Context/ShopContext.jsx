76e237f19b6b311796a485bd87db72abf473b068
Christopher Steven <christopherstevennunoo@gmail.com>
2024-01-31 15:44:15 +0000

    finished ShopContext

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -5,5 +5,13 @@ import all_product from '../Assets/all_product'
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
-    const contextValue = {}
+    const contextValue = {all_product};
+
+    return (
+        <ShopContext.Provider value={contextValue}>
+            {props.children}
+        </ShopContext.Provider>
+    )
 }
+
+export default ShopContextProvider;

af7743c35f48fe45c1e0e08397e1ac0ce1da159d
Christopher Steven <christopherstevennunoo@gmail.com>
2024-01-31 11:44:56 +0000

    finished Newsletter component content and styling

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import './NewsLetter.css'
 const NewsLetter = () => {
   return (
-    <div className='newsletter'>NewsLetter</div>
+    <div className='newsletter'>
+        <h1>Get Exclusive Offers On Your Email</h1>
+        <p>Suscribe to our newsletter and stay updated</p>
+        <div>
+            <input type="email" placeholder='Your Email Id'/>
+            <button>Suscribe</button>
+        </div>
+    </div>
   )
 }
 

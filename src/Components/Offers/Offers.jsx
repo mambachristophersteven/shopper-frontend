098ce1d9ad42d214ed9e2e5450907e07613385f7
Christopher Steven <christopherstevennunoo@gmail.com>
2024-01-31 10:05:23 +0000

    Offers component content added

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './Offers.css'
+import exclusive_image from '../../Assets/exclusive_image.png'
 const Offers = () => {
   return (
-    <div>Offers</div>
+    <div className='offers'>
+        <div className="offers-left">
+            <h1>EXCLUSIVE</h1>
+            <h1>OFFERS FOR YOU</h1>
+            <p>ONLY ON BEST SELLERS PRODUCTS</p>
+            <button>Check Now</button>
+        </div>
+        <div className="offers-right">
+            <img src={exclusive_image} alt="" />
+        </div>
+    </div>
   )
 }
 

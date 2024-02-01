0e60139c99f1ae968febdb15f87b4284f4180fdc
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-01 11:26:24 +0000

    LoginSignup page content added with styling file initialized

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account? <span>Log In</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the terms of use and privacy policy</p>
+        </div>
+      </div>
+    </div>
   )
 }

1cbd7d729031240d9d74cfd84a44e24f8f89743f
Christopher Steven <christopherstevennunoo@gmail.com>
2024-02-01 10:26:38 +0000

    adding banners as a prop for ShopCategory page

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './CSS/ShopCategory.css'
+import {ShopContext} from '../Context/ShopContext'
 export const ShopCategory = () => {
+
+  const {all_products} = useContext(ShopContext)
   return (
     <div>ShopCategory</div>
   )
